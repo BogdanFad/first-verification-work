deddad53ab50668938b20e8f28f53834aa6c9a83
Bogdan <bogdan.fad@yandex.ru>
2023-01-15 14:56:41 +0300

    Написал программу решения задача Task

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует 
+// новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+Console.WriteLine("Введите значения через пробел: ");
+string[] stringArray = Console.ReadLine()!.Split(' ');
+for (int i = 0; i < stringArray.Length; i++)
+{
+    Console.Write(stringArray[i] + " ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < stringArray.Length; i++)
+{
+    if (stringArray[i].Length <= 3)
+    {
+        Console.Write(stringArray[i] + " ");
+    }
+}
+Console.WriteLine();
